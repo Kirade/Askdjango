18a9ccc3c053a685ad40f0b0633291fd97c07cf0
허재영 <jay.jaeyoung@gmail.com>
2018-01-29 22:57:18 +0900

    Migration tutorials

--- tour_blog/blog/models.py
+++ tour_blog/blog/models.py
@@ -1,8 +1,19 @@
-from django.db import models
+import re
 
+from django.db import models
+from django.forms import ValidationError
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50,
+                              validators=[lnglat_validator],
+                              help_text='경도,위도 포멧으로 입력', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

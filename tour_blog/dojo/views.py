70e171c237412f4c2189b8ba17e0b9a076131e52
허재영 <jay.jaeyoung@gmail.com>
2018-01-29 20:31:17 +0900

    dojo cbv practice

--- tour_blog/dojo/views.py
+++ tour_blog/dojo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from django.views.generic import TemplateView
 
 def mysum(request, numbers):
     # request: HttpRequest

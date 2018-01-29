9bdd8279f3e063ea43acd5a88c2703f9387ae732
허재영 <jay.jaeyoung@gmail.com>
2018-01-29 19:03:30 +0900

    Create dojo function based view

--- tour_blog/dojo/views.py
+++ tour_blog/dojo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def mysum(request, numbers):
@@ -10,3 +10,24 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse("안녕하세요 {}. {}살이시네요.".format(name, age))
+
+
+def post_list1(request):
+    name = "공유"
+    return HttpResponse("""
+    <h1>Ask Django</h1>
+    <p>{name}</p>
+    <p> 장고 페이스메이커 !! </p>
+    """.format(name=name))
+
+
+def post_list2(request):
+    name = '공유'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery']
+    }, json_dumps_params={'ensure_ascii': False})
